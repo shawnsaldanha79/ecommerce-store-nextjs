c65bcf7498e6ad14a765f23e7df10ce498ec7cb7
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-03-10 12:39:57 +0530

    add schemas for product, product category, promotion code, promotion campaign

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,14 @@
+import { product } from './schemas/product'
+import { productCategory } from './schemas/product-category'
+import { promotionCampaign } from './schemas/promotion-campaign'
+import { promotionCode } from './schemas/promotion-code'
 import { type SchemaTypeDefinition } from 'sanity'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    promotionCode,
+    promotionCampaign,
+    productCategory,
+    product,
+  ],
 }
