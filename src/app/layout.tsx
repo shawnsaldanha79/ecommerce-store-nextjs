6b1bb85433186ef30d060b25bbe916d8413077f6
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-03-10 12:52:14 +0530

    add sanity live content integration and API token handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import { getCurrentSession } from "@/actions/auth";
+import { SanityLive } from "@/sanity/lib/live";
 
 const inter = Inter({ 
   subsets: ["latin"] 
@@ -26,6 +27,7 @@ const RootLayout = async ({
       >
           <Header user={user}/>
         {children}
+        <SanityLive />
       </body>
     </html>
   );
